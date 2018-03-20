a5f9516ad3e47682b26493b80ea715161d0acab7
Leo Calvis <leo.calvis@cgx-group.com>
2018-03-20 13:56:55 +0100

    simon hege : Bug fix in FAA Line/Line intersection: the computation on sphere was inaccurate, resulting in intersection not found.

--- GeoFormulas/CrsIntersect.cpp
+++ GeoFormulas/CrsIntersect.cpp
@@ -51,30 +51,68 @@ namespace GeoCalcs {
     {
         LLPoint pt1 = llPt1;
         LLPoint pt2 = llPt2;
-
+        double dAz13 = az13;
+		double dAz23 = az23;
         InverseResult result;
         DistVincenty(pt1, pt2, result);
+        double dist12 = result.distance;
+		double crs12 = result.azimuth;
+		double crs21 = result.reverseAzimuth;
 
-        double angle1 = fabs(SignAzimuthDifference(az13, result.azimuth));
-        double angle2 = fabs(SignAzimuthDifference(result.reverseAzimuth, az23));
-        if (angle1 < 0.0 && angle2 < 0.0)
-        {
-            angle1 = -angle1;
-            angle2 = -angle2;
-        }
+        double angle1 = SignAzimuthDifference(crs12, dAz13);
+		double angle2 = SignAzimuthDifference(dAz23, crs21);
 
         if (sin(angle1) == 0.0 && sin(angle2) == 0.0)
             return false;
 
-        // step 7
-        // locate approx intersection of point3 using spherical earth model. Section 3.2
+        if(sin(angle1)*sin(angle2)<0)
+		{
+			//The courses lay on opposite sides of the line between p1 and p2. One of the course must be reversed.
+			if(fabs(angle1)>fabs(angle2))
+			{
+				dAz13 = Mod(dAz13 + M_PI, M_2PI);
+				angle1 = SignAzimuthDifference(crs12, dAz13);
+			}
+			else
+			{
+				dAz23 = Mod(dAz23 + M_PI, M_2PI);
+				angle2 = SignAzimuthDifference(dAz23, crs21);
+			}
+		}
+		
+		angle1 = fabs(angle1);
+		angle2 = fabs(angle2);
 
-        const double cosA = cos(angle1);
-        const double sinA = sin(angle1);
-        const double cosB = cos(angle2);
-        const double sinB = sin(angle2);
+        double cosA = cos(angle1);
+        double sinA = sin(angle1);
+        double cosB = cos(angle2);
+        double sinB = sin(angle2);
+        
+        //Test if the intersection is backcourse. In this case, reverse both courses.
+		//We check the sign of the Napier's analogies result (cf. https://en.wikipedia.org/wiki/Solution_of_triangles#A_side_and_two_adjacent_angles_given_.28spherical_ASA.29 )
+		double tanc2 = tan(dist12 / (2*kSphereRadius));
+		double cotc2 = 1/tanc2;
+		double sinAplusB = sin(angle1+angle2);
+		double sinAminusB = sin(angle1-angle2);
+        
+		double b_ = kSphereRadius * atan( 2*sinB/(cotc2*sinAplusB+tanc2*sinAminusB) );
+		if(sinB/(cotc2*sinAplusB+tanc2*sinAminusB) <= 0) //ie. b <= 0
+		{
+			dAz13 = Mod(dAz13 + M_PI, M_2PI);
+			angle1 = fabs(SignAzimuthDifference(crs12, dAz13));
+			dAz23 = Mod(dAz23 + M_PI, M_2PI);
+			angle2 = fabs(SignAzimuthDifference(dAz23, crs21));
+		
+			cosA = cos(angle1);
+			sinA = sin(angle1);
+			cosB = cos(angle2);
+			sinB = sin(angle2);
+		}
 
-        const double C = acos(-cosA * cosB + sinA * sinB * cos(result.distance / kSphereRadius));
+		// step 7
+		// locate approx intersection of point3 using spherical earth model. Section 3.2
+
+        const double C = acos(-cosA * cosB + sinA * sinB * cos(dist12 / kSphereRadius));
         const double cosC = cos(C);
         const double sinC = sin(C);
         const double a = kSphereRadius * acos((cosA + cosB * cosC) / (sinB * sinC));
@@ -83,7 +121,7 @@ namespace GeoCalcs {
         if (std::isnan(a) || std::isnan(b))
             return false;
 
-        llIntersect = DestVincenty(pt1, az13, b);
+        llIntersect = DestVincenty(pt1, dAz13, b);
         DistVincenty(pt1, llIntersect, result);
         dist13 = result.distance;
 
@@ -97,8 +135,8 @@ namespace GeoCalcs {
             llIntersect = llInv;
             dist13 = result.distance;
             az31 = result.reverseAzimuth;
-            az13 = az13 + M_PI;
-            az23 = az23 + M_PI;
+            dAz13 = dAz13 + M_PI;
+            dAz23 = dAz23 + M_PI;
         }
 
         DistVincenty(pt2, llIntersect, result);
@@ -106,22 +144,22 @@ namespace GeoCalcs {
 
         if (dist13 < NmToMeters(1))
         {
-            pt1 = DestVincenty(pt1, az13 + M_PI, NmToMeters(1.0));
+            pt1 = DestVincenty(pt1, dAz13 + M_PI, NmToMeters(1.0));
             DistVincenty(pt1, llIntersect, result);
-            az13 = result.azimuth;
+            dAz13 = result.azimuth;
         }
         if (dist23 < NmToMeters(1))
         {
-            pt2 = DestVincenty(pt2, az23 + M_PI, NmToMeters(1.0));
+            pt2 = DestVincenty(pt2, dAz23 + M_PI, NmToMeters(1.0));
             DistVincenty(pt2, llIntersect, result);
-            az23 = result.azimuth;
+            dAz23 = result.azimuth;
         }
 
         bool bSwapped = false;
         if (dist23 < dist13)
         {
             std::swap(pt1, pt2);
-            std::swap(az13, az23);
+            std::swap(dAz13, dAz23);
             dist13 = dist23;
             bSwapped = true;
         }
@@ -130,13 +168,13 @@ namespace GeoCalcs {
         distarray[0] = dist13;
         distarray[1] = 1.01 * dist13;
 
-        llIntersect = DestVincenty(pt1, az13, dist13);
+        llIntersect = DestVincenty(pt1, dAz13, dist13);
         DistVincenty(pt2, llIntersect, result);
-        errarray[0] = SignAzimuthDifference(result.azimuth, az23);
+        errarray[0] = SignAzimuthDifference(result.azimuth, dAz23);
 
-        llIntersect = DestVincenty(pt1, az13, distarray[1]);
+        llIntersect = DestVincenty(pt1, dAz13, distarray[1]);
         DistVincenty(pt2, llIntersect, result);
-        errarray[1] = SignAzimuthDifference(result.azimuth, az23);
+        errarray[1] = SignAzimuthDifference(result.azimuth, dAz23);
 
         const int nMaxCount = 15;
         double dErr = 0.0;
@@ -147,10 +185,10 @@ namespace GeoCalcs {
             if (std::isnan(dist13))
                 break;
 
-            LLPoint newPt = DestVincenty(pt1, az13, dist13);
+            LLPoint newPt = DestVincenty(pt1, dAz13, dist13);
             DistVincenty(pt2, newPt, result);
             errarray[0] = errarray[1];
-            errarray[1] = SignAzimuthDifference(result.azimuth, az23);
+            errarray[1] = SignAzimuthDifference(result.azimuth, dAz23);
             distarray[0] = distarray[1];
             distarray[1] = dist13;
 
@@ -168,7 +206,7 @@ namespace GeoCalcs {
         if (bSwapped)
         {
             std::swap(pt1, pt2);
-            std::swap(az13, az23);
+            std::swap(dAz13, dAz23);
             dist13 = dist23;
         }
         DistVincenty(llIntersect, pt1, result);
